4b1351b1e2264b800174209e530528401040c63c
Ego Dafma Dasa <egodasa@gmail.com>
2018-09-10 10:03:53 +0700

    Setting database

--- config/database.php
+++ config/database.php
@@ -3,13 +3,8 @@
 //Load library database
 require_once("db.php");
 
-$username = "root";
-$password = "";
-$server = "localhost";
-$database = "events";
-
 // Instance database yaitu variabel $db
 $db = new Sparrow();
 $db->show_sql = true;
-$db->setDb("mysqli://".$username.":".$password."@".$server."/".$database);
+$db->setDb(getenv("DATABASE_URL"));
 ?>
