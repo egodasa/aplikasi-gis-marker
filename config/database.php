90adc4c1a1bbc106ab3093bba1b2d71b519fe501
Ego Dafma Dasa <egodasa@gmail.com>
2018-09-10 10:17:53 +0700

    Set database

--- config/database.php
+++ config/database.php
@@ -7,7 +7,7 @@ $username = "ijmyzlwbtnvjlp";
 $password = "5a7b14c8dfcb7d5f9417ea0296d6c3d1a51ec5e26a087a08f0a43dbcebe757ce";
 $server = "ec2-184-73-174-171.compute-1.amazonaws.com";
 $database = "d775vshpuv9cs0";
-
+// miaw
 // Instance database yaitu variabel $db
 $db = new Sparrow();
 $db->show_sql = true;
